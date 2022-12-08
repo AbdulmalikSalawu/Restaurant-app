185a72f5f4208b11e9e978d6b91280af89abcab6
Salawu Abdulmalik <salawuabdulmalik90@gmail.com>
2022-12-07 16:49:00 -0800

    Started work on the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,12 +4,14 @@ import mainfood from '../Assets/mainfood.jpg'
 
 function Home() {
   return (
-    <div className='row'>
+    <div className='row '>
       <div className='col-sm-12 col-md-10 col-lg-7'>
           <h1>Cooking gone wrong?, <span>Order food </span>you <span> love</span> delivered now!</h1>
+          <p className='pe-lg-5'>Anything, anytime, anywhere, we've got you!. Helping you enjoy confortable and health food on the go</p>
+          <button className='orderButton px-3 py-2 fs-5'>Order now</button>
       </div>
       <div className='col-lg-5'>
-          <img src={mainfood} alt='jollof picture' />
+          <img className='mainfood ms-lg-4' src={mainfood} alt='jollof picture' />
       </div>
     </div>
   )
